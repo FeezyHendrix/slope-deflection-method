8cd20f8b0b52df9647319c6610fa200775b28278
AbdulHafeez AbdulRaheem <switchdevelop1@gmail.com>
2024-01-30 02:41:35 +0100

    feat: update

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify, make_response, request
 
+from engine import calculate_beam_properties
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +11,22 @@ def home_page():
 @app.route("/calculate", methods=['POST'])
 def calculate():
   data = request.get_json()
+    
+
+    # Extract parameters from the request
+  number_of_supports = data.get('number_of_supports')
+  number_of_internal_joints = data.get('number_of_internal_joints')
+  span_data = data.get('span_data')
+  settlement_positions = data.get('settlement_positions')
+  settlement_on_beam = data.get('settlement_on_beam')
+  first_node_fixed = data.get('first_node_fixed')
+  last_node_fixed = data.get('last_node_fixed')
+
+  # Call the function
+  result = calculate_beam_properties(number_of_supports, number_of_internal_joints, span_data, settlement_positions, settlement_on_beam, first_node_fixed, last_node_fixed)
+
+  # Return the result as a JSON response
+  return jsonify(result)
 
 
 if __name__ == '__main__':
